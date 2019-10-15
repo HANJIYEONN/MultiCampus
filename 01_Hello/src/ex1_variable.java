a67ba2b47b370b3316ca57652f65184bec1ef720
HANJIYEONN <onedelay1@naver.com>
2019-10-15 11:43:42 +0900

    2.TEST02

---
+++ 01_Hello/src/ex1_variable.java
@@ -0,0 +1,29 @@
+
+public class ex1_variable {
+
+	public static void main(String[] args) {
+		/*
+		 데이터타입
+		 */
+		//정수형
+		
+		int i = 26; //10진수를 26으로 표현
+		int oi= 032; // 8진수로 26으로 표현
+		int xi = 0x1a; //16진수로 26을 표현
+		
+		System.out.println(i);
+		System.out.println(oi);
+		System.out.println(xi);
+		
+		//long : 부호가 있는 정수
+		//64bit로 정수를 저장
+		//수치가 큰 데이터를 다루는 프로그램에서 long 형 사용
+		
+		//실수형
+		//소수점이 있는 데이터를 저장 
+		//float : 32bit
+		//double : 64bit
+
+	}
+
+}
