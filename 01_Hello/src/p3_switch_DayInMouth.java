6dac36ffe00fac7c6178f87c54d0eacaab340970
HANJIYEON <47732135+HANJIYEONN@users.noreply.github.com>
2019-10-15 21:03:56 +0900

    Update p3_switch_DayInMouth.java

--- 01_Hello/src/p3_switch_DayInMouth.java
+++ 01_Hello/src/p3_switch_DayInMouth.java
@@ -17,26 +17,37 @@ public class p3_switch_DayInMouth {
 	public static void main(String[] args) {
 		System.out.println("일수를 알고싶은 달을 입력하세요");
 		Scanner sc = new Scanner(System.in);
-		
 		int month = sc.nextInt();
-		String day = null;
-		
-		String basic1 = "2019년";
-		String basic2 = "";
+		int thisYear = 2019;
+		String day = "error";
 		
 		if (month == 1|| month == 3 || month == 5 ||month == 7 || month == 9 || month ==11 ) {
 			day = "odd";
-		} day = "even";
-		
+		} else if (month == 4 || month == 6 ||month == 8 || month == 10 || month == 12){
+			day = "even";
+		} else if (month == 2 && thisYear % 4 == 0) {
+			day = "leaf";
+		} else if(month == 2 && thisYear % 4 != 0) {
+			day = "notLeaf";
+		} 
+			
+			
+//	  	2019년 8월의 일수는 31일
 		switch (day) {
 		case "odd":
-			System.out.println("a");
-		break;								
+			System.out.println(thisYear+"년 "+month+"월의 일수는 30일");
+			break;								
 		case "even":
-		System.out.println("b");
+			System.out.println(thisYear+"년 "+month+"월의 일수는 31일");
+			break;		
+		case "leaf":
+			System.out.println(thisYear+"년 "+month+"월의 일수는 28일");
+			break;		
+		case "notLeaf":
+			System.out.println(thisYear+"년 "+month+"월의 일수는 29일");
+			break;	
 		default:
 		System.out.println("월을 잘못 입력하셨습니다.");
-		break;
 	}
 		
 	}
