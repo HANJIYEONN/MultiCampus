a5e4066971a1c34ebb6ddca245ba7fd88f933f72
HANJIYEONN <onedelay1@naver.com>
2019-10-16 13:42:50 +0900

    06

--- src/e2_array.java
+++ src/e2_array.java
@@ -1,7 +1,12 @@
 
 public class e2_array {
 	public static void main(String[] args) {
-		
+		//20명의 학생 정보를 저장해야한다면?
+		int s1;
+		//우리가 생각하는 s1은 학생 정보를 저잘하기 위한변수
+		//s1과 s2는 연관성이 있음
+		//num : 수를 저장하기 위한 변수
+		//s1이나 num과 
 	}
 
 }
